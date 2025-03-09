ef2fd50eeeba125e455b8726a96442b8505288fe
Kalil Sekeff <ksekeff@gmail.com>
2025-03-09 18:11:37 -0300

    Criado o projeto com seus respectivos app e iniciando os templates

---
+++ suppliers/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Supplier'
+        verbose_name_plural = 'Suppliers'
+        ordering = ['name']
+
+
+    def __str__(self):
+        return self.name
