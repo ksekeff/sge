ca83feb5309712573966b3f9755c8ac5e552d9f7
Kalil Sekeff <ksekeff@gmail.com>
2025-03-11 21:43:51 -0300

    Criação de novos html, novas rotas, views e ajuste nos forms

--- brands/urls.py
+++ brands/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import BrandListView
+from .views import BrandListView, BrandCreateView, BrandDetailView
 
 urlpatterns = [
-    path('brands/list/', BrandListView.as_view(), name='brands_list'),  
+    path('brands/list/', BrandListView.as_view(), name='brand_list'),
+    path('brands/create/', BrandCreateView.as_view(), name='brand_create'),
+    path('brands/<int:pk>/detail/', BrandDetailView.as_view(), name='brand_detail'),
 ]
--- brands/views.py
+++ brands/views.py
@@ -1,13 +1,30 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
+from . import models, forms
 from .models import Brand
-
 
 class BrandListView(ListView):
     model = Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
     
-    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+        
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
     
 
+class BrandCreateView(CreateView):
+    model = Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = Brand
+    template_name = 'brand_detail.html'
     
