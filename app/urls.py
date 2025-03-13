0efc3deb9a418a430cdeb69cce1cb07cbc03608e
Kalil Sekeff <ksekeff@gmail.com>
2025-03-12 23:04:05 -0300

    Adicionei mais um CRUD completo de dois apps

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('brands.urls')),
+    path('', include('categories.urls')),
+    path('', include('suppliers.urls')),
 ]
